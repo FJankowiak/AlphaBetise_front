2760f2e43be09db123cc5db08ffbc293e86fdb2e
Crissy31 <christine.andreck@gmail.com>
2023-01-10 17:40:24 +0100

    creation arborescence dossiers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksListComponent } from './common/components/books-list/books-list.component';
+import { AddBookComponent } from './common/components/add-book/add-book.component';
+import { BookDetailsComponent } from './common/components/book-details/book-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksListComponent,
+    AddBookComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
