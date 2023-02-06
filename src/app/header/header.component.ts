da73e11c5687791e34c4624afe2fa91892172975
Andynator1 <andyjeanty249@gmail.com>
2023-02-06 12:26:45 +0100

    Header Alphabetise Ajout composant header, users et le routing des composants

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hedear',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HedearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
