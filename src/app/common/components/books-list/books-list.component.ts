ba78bb8d8fa06ecdf7052c10002ed631c10c9238
Crissy31 <christine.andreck@gmail.com>
2023-02-07 10:25:30 +0100

    resolution conflit app.module.ts

--- src/app/common/components/books-list/books-list.component.ts
+++ src/app/common/components/books-list/books-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-books-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books-list.component.css']
 })
 export class BooksListComponent implements OnInit {
+  books: Book[] =[];
 
-  constructor() { }
+  constructor(private bookService:BookService) { }
 
   ngOnInit(): void {
+    this.getBooks();
+  }
+  getBooks() {
+    this.bookService.getListBooks()
+    .subscribe(data =>{
+      this.books = data;
+    })
   }
 
 }
