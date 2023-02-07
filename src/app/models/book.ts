08f5b5d02ab49a1e3b355adb29704eb82eff7009
Crissy31 <christine.andreck@gmail.com>
2023-02-07 09:59:05 +0100

    component book-list

---
+++ src/app/models/book.ts
@@ -0,0 +1,17 @@
+export class Book {
+ constructor(public codeEAN:number,
+    public codeISBN:string,
+    public titre: string,
+    public images: string,
+    public resume: string,
+    public collection:string,
+    public dateParution: Date,
+    public quantite: number,
+    public prix: number,
+    public editeur: string,
+    public auteurs:string,
+    public notesBiographiques:string
+
+  ) { }
+
+}
---
+++ src/app/services/book.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+ private baseUrl = 'http://localhost:8080/livres/books-list';
+
+  constructor(private httpClient: HttpClient) { }
+
+  // return books list
+  getListBooks() :Observable<Book[]>{
+    return this.httpClient.get<Book[]>(`${this.baseUrl}`);
+  }
+}
