a33eb0fb1a607aac72a20a862fa33e80c27c0993
FJanko <fab.jankowi@gmail.com>
2023-02-08 10:35:23 +0100

    resolution conflit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksListComponent } from './common/components/books-list/books-list.component';
@@ -8,7 +8,8 @@ import { AddBookComponent } from './common/components/add-book/add-book.componen
 import { BookDetailsComponent } from './common/components/book-details/book-details.component';
 import { HeaderComponent } from  './header/header.component';
 import { UsersComponent } from './common/components/users/users.component';
-import { LoginComponent } from './auth/login/login.component';
+//import { BookService } from './services/book.service';
+//import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,10 +19,10 @@ import { LoginComponent } from './auth/login/login.component';
     BookDetailsComponent,
     HeaderComponent,
     UsersComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
